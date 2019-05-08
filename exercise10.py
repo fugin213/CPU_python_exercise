1ad90e9e2146e39c0ffe5cb0aa00e9b5018622a0
fugin213 <50404004+fugin213@users.noreply.github.com>
2019-05-08 18:19:12 +0800

    Update exercise10.py

--- exercise10.py
+++ exercise10.py
@@ -3,10 +3,3 @@ fr=open("read.txt","r")
 a=fr.read()
 print("The content of the file is:" , a)
 b_split=a.split(" ")
-#print(b_split)
-b=[2,3,4,5,3,2,4]
-fw=open("write.txt","w")
-for i in range(len(b)):
-    fw.writelines(str(b[i])+" ")
-    print(str(b[i])+" is written to the file")
-fw.write("\n")
